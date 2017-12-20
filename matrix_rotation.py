669db5b86c2d061b12280d40ceb3b045473ba87f
Anshul Garg <anshulgarg.garg@gmail.com>
2017-12-20 06:12:50 +0530

    fixes a minor bug in matrix rotation logic

--- matrix_rotation.py
+++ matrix_rotation.py
@@ -6,23 +6,11 @@ def rotate_matrix(matrix, n):
     result = deepcopy(matrix)
     for i in range(n):
         for j in range(n-1, -1, -1):
-            result[i][j] = matrix[2-j][i]
+            result[i][n-j-1] = matrix[j][i]
     return result
 
-
-matrix = [[1,2,3],[4,5,6],[7,8,9]]
-n = 3
-
-# result[0][2] == 1 == matrix[0][0]
-# result[0][1] == 4 == matrix[1][0]
-# result[0][0] == 7 == matrix[2][0]
-# result[1][2] == 2 == matrix[0][1]
-# result[1][1] == 5 == matrix[1][1]
-# result[1][0] == 8 == matrix[2][1]
-# result[2][2] == 3 == matrix[0][2]
-# result[2][1] == 5 == matrix[1][2]
-# result[2][0] == 9 == matrix[2][2]
-# result[i][j] = matrix[2-j][i]
+matrix = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]]
+n = 4
 
 print (matrix)
 print (rotate_matrix(matrix, n))
