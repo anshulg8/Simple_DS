5d1fc1fff740671f1746f4aaa9a89c98dcfe8533
Anshul Garg <anshulgarg.garg@gmail.com>
2017-12-30 21:08:20 +0530

    adds comments about complexity

--- ctci-ransom-note.py
+++ ctci-ransom-note.py
@@ -1,6 +1,11 @@
 # https://www.hackerrank.com/challenges/ctci-ransom-note/problem
 # Hash Tables: Ransom Note
 
+# Instead of searching through the entire magazine word list again and again[O(MxN)],
+# it's possible to hash each word in the magazine and store a count along with it(word_counts).
+# Then, when you want to check if a word is in the magazine, you can check the hash in O(1)
+# time for a result of O(N) to construct the hash and O(M) to check all m words, which is a final O(N+M).
+
 from collections import Counter
 
 def ransom_note(magazine, ransom):
