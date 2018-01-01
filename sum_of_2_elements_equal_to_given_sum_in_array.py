91c3c852838ada54524bb98b51964e0645e0a1e2
Anshul Garg <anshulgarg.garg@gmail.com>
2018-01-01 21:15:58 +0530

    Given an array and a number, check for pair in array with sum as the number

---
+++ sum_of_2_elements_equal_to_given_sum_in_array.py
@@ -0,0 +1,33 @@
+# Given an array A[] and a number x, check for pair in A[] with sum as x
+
+# Requires list to be sorted
+def abc(arr, count):
+    i = 0
+    j = len(arr)-1
+    while(i<j):
+        s = int(arr[i]) + int(arr[j])
+        if s == count:
+            return True
+        elif s < count:
+            i += 1
+        elif s > count:
+            j -= 1
+    return False
+
+# Doesn't require list to be sorted
+def abcd(arr, count):
+    comp = []
+    for i in range(len(arr)-1):
+        if arr[i] in comp:
+            return True
+        else:
+            comp.append(count - arr[i])
+    return False
+
+# array = input().strip().split()
+array = list(map(int, input().strip().split()))
+result = int(input())
+# array = [1,2,3,4,5,6]
+# result = 6
+print (abc(array, result))
+print (abcd(array, result))
