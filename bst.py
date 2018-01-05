72b75d0fc90c481c1d6f6c74e8f9f870da21d079
Anshul Garg <anshulgarg.garg@gmail.com>
2018-01-05 08:56:25 +0530

    adds remove method for bst

--- bst.py
+++ bst.py
@@ -95,6 +95,107 @@ class Tree:
         else:
             return -1
 
+    def remove(self, data):
+        if self.root is None:
+            return False
+
+        elif self.root.value == data:
+
+            if self.root.leftChild is None and self.root.rightChild is None:
+                self.root = None
+
+            elif self.root.leftChild and self.root.rightChild is None:
+                self.root = self.leftChild
+
+            elif self.root.leftChild is None and self.root.rightChild:
+                self.root = self.rightChild
+
+            elif self.root.leftChild and self.root.rightChild:
+                delNodeParent = self.root
+                delNode = self.root.rightChild
+                while delNode.leftChild:
+                    delNodeParent = delNode
+                    delNode = delNodeParent.leftChild
+
+                self.root.value = delNode.value
+                if delNode.rightChild:
+                    if delNodeParent.value > delNode.value:
+                        delNodeParent.leftChild = delNode.rightChild
+
+                    elif delNodeParent.value < delNode.value:
+                        delNodeParent.rightChild = delNode.rightChild
+
+                    else:
+                        if delNodeParent.value > delNode.value:
+                            delNodeParent.leftChild = None
+                        elif delNodeParent.value < delNode.value:
+                            delNodeParent.rightChild = None
+
+                return True
+
+        parent = None
+        node = self.root
+
+        # find node to remove
+        while node and node.value != data:
+            parent = node
+            if data < node.value:
+                node = node.leftChild
+            if data > node.value:
+                node = node.rightChild
+
+        # Case 1: data not found
+        if node is None or node.value != data:
+            return False
+
+        # Case 2: remove-node has no children
+        elif node.leftChild is None and node.rightChild is None:
+            if data < parent.value:
+                parent.leftChild = None
+            if data > parent.value:
+                parent.rightChild = None
+            return True
+
+        # Case 3: remove-node has left child only
+        elif node.leftChild and node.rightChild is None:
+            if data < parent.value:
+                parent.leftChild = node.leftChild
+            if data > parent.value:
+                parent.rightChild = node.leftChild
+            return True
+
+        # Case 4: remove-node has right child only
+        elif node.rightChild and node.leftChild is None:
+            if data < parent.value:
+                parent.leftChild = node.rightChild
+            if data > parent.value:
+                parent.rightChild = node.rightChild
+            return True
+
+        # Case 5: remove-node has both left and right children
+        # You replace remove-node with either the left most child on its right side,
+        # or the right most child on its left side.
+        # You then delete the child from the bottom that it was replaced with.
+
+        else:
+            delNodeParent = node
+            delNode = node.rightChild
+            while delNode.leftChild:
+                delNodeParent = delNode
+                delNode = delNode.leftChild
+
+            node.value = delNode.value
+            if delNode.rightChild:
+                if delNodeParent.value > delNode.value:
+                    delNodeParent.leftChild = delNode.rightChild
+                elif delNodeParent.value < delNode.value:
+                    delNodeParent.rightChild = delNode.rightChild
+            else:
+                if delNodeParent.value > delNode.value:
+                    delNodeParent.leftChild = None
+                else:
+                    delNodeParent.rightChild = None
+
     def preorder(self):
         print ("PreOrder")
         self.root.preorder()
@@ -115,6 +216,7 @@ bst.insert(7)
 bst.insert(5)
 bst.insert(9)
 bst.insert(11)
+bst.remove(11)
 
 bst.preorder()
 bst.postorder()
